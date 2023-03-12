09f55f5a29127be8582d0dfc759ce076e2c8d2b2
Raul <jarquinr121@csu.fullerton.edu>
2023-03-12 12:08:02 -0700

    Add scan fucntion & 4 more test cases

--- Tests/test_primary.cpp
+++ Tests/test_primary.cpp
@@ -1,3 +1,6 @@
+//Includes Primary, Term, Print & scan function. Comment all functions besides the one you want to test in main.
+//Write your input in input.txt & run like an oridnary cpp file 
+
 #include <cstdio>
 #include <iostream>
 #include <vector>
@@ -8,11 +11,13 @@ using namespace std;
 void test_Primary();
 void test_term();
 void test_Print();
+void test_Scan();
 int main(int argc, char *argv[])
 {
   //test_Primary();
   //test_term();
-  test_Print();
+  //test_Print();
+  test_Scan();
   return 0;
 }
 void test_Primary()
@@ -108,6 +113,41 @@ void test_Print()
         cout << "Failed: Testing of R20. <Print> " << endl;
     } else {
         cout << "Passed: Testing of R20. <Print> " << endl;
+    }
+
+    cout << "\nHere are all the tokens in the vector:" << endl;
+    for (auto token : all_tokens) {
+        token.token_print_helper();
+    }
+
+    input_file.close();
+
+    return;
+}
+
+void test_Scan()
+{
+    ifstream input_file("input.txt");
+    vector<token_323> all_tokens;
+    int location = 0;
+    
+    bool test_results = false;
+    int end_file;
+
+    while (!input_file.eof()) {
+        all_tokens.push_back(lexer_323(input_file));
+        end_file = input_file.peek();
+        if (end_file == EOF) {
+            break;
+        }
+    }
+
+    test_results = procedure_Scan(all_tokens, location);
+
+    if (test_results == false) {
+        cout << "Failed: Testing of R21. <Scan> " << endl;
+    } else {
+        cout << "Passed: Testing of R21. <Scan> " << endl;
     }
 
     cout << "\nHere are all the tokens in the vector:" << endl;
